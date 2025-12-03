76d857706797fe1efeb8361971f681fd6c34b2de
Dev aadi <adityasaini35947@gmail.com>
2025-12-03 19:25:51 +0530

    Frontend + Backend initial upload

---
+++ backend/models/posts.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  body: {
+    type: String,
+    require: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  media: {
+    typr: String,
+    default: "",
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  fileType: {
+    type: String,
+    default: "",
+  },
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
