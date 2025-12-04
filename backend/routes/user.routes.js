b8b9aafc1b583c26b682515cf32fe93d66a6bcb1
Dev aadi <adityasaini35947@gmail.com>
2025-12-05 01:53:03 +0530

    modified:   backend/routes/user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,40 @@
-import {Router} from "express";
-import { register, login} from "../controllers/user.controller.js";
+import { Router } from "express";
+import {
+  uploadProfilePicture,
+  register,
+  login,
+  updateUserProfile,
+  getUserAndProfile,
+  updateProfileData,
+  getAllUserProfile,
+  downloadProfile,
+} from "../controllers/user.controller.js";
 
+import multer from "multer";
 
 const router = Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router
+  .route("/update_profile_picture")
+  .post(upload.single("profile_picture"), uploadProfilePicture);
+
 router.route("/register").post(register);
 router.route("/login").post(login);
-
+router.route("/user_update").post(updateUserProfile); // update: username, email
+router.route("/get_user_and_Profile").get(getUserAndProfile);
+router.route("/update_profile_data").post(updateProfileData);
+router.route("/user/get_all_users").get(getAllUserProfile);
+router.route("/user/download_resume").get(downloadProfile);
 
 export default router;
