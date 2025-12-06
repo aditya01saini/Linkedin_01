e415b264bf6e55b1ae713424bd2bf022e2a260f1
Dev aadi <adityasaini35947@gmail.com>
2025-12-07 00:07:33 +0530

    modified:   backend/routes/posts.routes.js

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import { activeCheck } from "../controllers/posts.controller.js";
+import { activeCheck, createPost, getAllPost,deletePost } from "../controllers/posts.controller.js";
 
 const router = Router();
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.route("/").get(activeCheck);
-
+router.route("/post").post(upload.single("media"), createPost);
+router.route("/posts").get(getAllPost);
+router.route("delete_Post").post(deletePost);
 
 export default router;
