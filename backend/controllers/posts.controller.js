13a3738b32914fead9202250826ddec437b6bade
Dev aadi <adityasaini35947@gmail.com>
2025-12-07 00:05:41 +0530

    modified:   backend/controllers/posts.controller.js

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,6 +1,70 @@
-
+import Post from "../models/posts.model.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const activeCheck = async (req, res) => {
   return res.status(200).json({ message: "RUNNING" });
 };
 
+export const createPost = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not fund!" });
+    }
+
+    const post = new Post({
+      userId: user.id,
+      body: req.body.body,
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+
+    await post.save();
+
+    return res.status(200).json({ message: "Post Created!" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getAllPost = async (req, res) => {
+  try {
+    const posts = await Post.find().populate(
+      "userId",
+      "name username email password"
+    );
+    return res.json({ posts });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { token, post_id } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    const post = await Post.findOne({ _id: post._id });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found!" });
+    }
+
+    if (post.userId.toString() !== user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    await Post.deletePost({_id: post_id});
+
+    return res.json({message: "Post Deleted"});
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
