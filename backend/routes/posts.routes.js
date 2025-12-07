bf6e3c7c3e97b634f09c806237e98c03a4523340
Dev aadi <adityasaini35947@gmail.com>
2025-12-08 00:26:58 +0530

    modified:   backend/routes/posts.routes.js

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { activeCheck, createPost, getAllPost,deletePost } from "../controllers/posts.controller.js";
+import { activeCheck, createPost, getAllPost,deletePost, commentPost, get_comments_by_post, delete_comment_of_user, increment_likes } from "../controllers/posts.controller.js";
 
 const router = Router();
 import multer from "multer";
@@ -18,6 +18,10 @@ const upload = multer({ storage: storage });
 router.route("/").get(activeCheck);
 router.route("/post").post(upload.single("media"), createPost);
 router.route("/posts").get(getAllPost);
-router.route("delete_Post").post(deletePost);
+router.route("delete_post").post(deletePost);
+router.route("/comment").post(commentPost);
+router.route("/get_comments").get(get_comments_by_post);
+router.route("/delete_comment").delete(delete_comment_of_user);
+router.route("/increment_post_like").post(increment_likes);
 
 export default router;
