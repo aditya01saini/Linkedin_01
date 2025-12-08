7ff69f62ef9aa6955662a7eb547673e1f1016a15
Dev aadi <adityasaini35947@gmail.com>
2025-12-09 00:11:29 +0530

    modified:   frontend/src/config/redux/store.js

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { actionAsyncStorage } from "next/dist/server/app-render/action-async-storage.external"
+import authReducer from "./reducer/authReducer"
+
+// 
+// steps for state management
+// submit action
+// handle action in tis reducer
+// register here -> reducer
+// 
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer
+    }
+})
