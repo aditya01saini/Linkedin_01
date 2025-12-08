277c20a3179dc33939cea6079464d7655d5489f3
Dev aadi <adityasaini35947@gmail.com>
2025-12-09 00:12:55 +0530

    add : frontend/src/config/redux/reducer

---
+++ frontend/src/config/redux/reducer/authReducer/index.js
@@ -0,0 +1,64 @@
+import { loginUser, registerUser } from "../../action/authAction"
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  loggedIn: false,
+  message: "",
+  profileFetched: false,
+  connections: [],
+  connectionRequest: [],
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    handdleLoginUser: (state) => {
+      state.message = "hello";
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true
+        state.message = "Knocking the door..."
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.loggedIn = true;
+        state.message = "Login is Successfull";
+      })
+
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+        state.message = "Registring You";
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.register = true;
+        state.message = "Registration is  Successfull";
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export default authSlice.reducer;
