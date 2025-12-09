e192a0ed6a9198f706bee912ca4e94ce5a1c270b
Dev aadi <adityasaini35947@gmail.com>
2025-12-10 01:30:14 +0530

    add: frontend/src/pages/login

---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import UserLayout from '@/layout/UserLayout';
+import React from 'react';
+
+export default function LoginComponent() {
+  return (
+    <div>
+      <UserLayout>
+        <h2>login</h2>
+      </UserLayout>
+    </div>
+  );
+}
