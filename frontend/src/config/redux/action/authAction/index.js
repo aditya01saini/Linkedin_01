968954d8ed12f0f7698c0db055f0da9838db5148
Dev aadi <adityasaini35947@gmail.com>
2025-12-11 02:44:16 +0530

    modified:   frontend/src/config/redux/action/authAction/index.js

--- frontend/src/config/redux/action/authAction/index.js
+++ frontend/src/config/redux/action/authAction/index.js
@@ -26,5 +26,19 @@ export const loginUser = createAsyncThunk(
 
 
 export const registerUser = createAsyncThunk("user/register", async(user, thunkAPI) => {
+  try{
+    const request = await clientServer.post("/register", {
+      username: user.username,
+      password: user.password,
+      email: user.email,
+      name: user.name,
+
+    });
+
+
+
+  }catch(error) {
+    return thunkAPI.rejectWithValue(error.response.data);
+  }
 
 })
