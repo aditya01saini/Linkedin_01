1d315dd6088956c0bc05c5e22e4b5211621b496f
Dev aadi <adityasaini35947@gmail.com>
2025-12-12 02:38:00 +0530

    modified:   frontend/src/config/redux/action/postAction/index.js

---
+++ frontend/src/config/redux/action/postAction/index.js
@@ -0,0 +1,20 @@
+import { clientServer } from "@/config";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+
+
+export const getAllPosts = createAsyncThunk(
+    "post/getAllPosts",async(__, thunkAPI) => {
+        try{
+            const response = await clientServer.get("/posts");
+
+            return thunkAPI.fulfillWithValue(response.data);
+
+        }catch(error){
+            return thunkAPI.rejectWithValue(error.response.data);
+        }
+
+    }
+
+)
