d2bf00cc2467899d960ce1654d967bc73c21a4a4
Dev aadi <adityasaini35947@gmail.com>
2025-12-12 02:38:55 +0530

    modified:   frontend/src/pages/dashboard/index.jsx

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,21 +1,31 @@
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { getAboutUser } from "@/config/redux/action/authAction";
+import { getAllPosts } from "@/config/redux/action/postAction";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function dashboard() {
-
   const router = useRouter();
+  const dispatch = useDispatch();
+
+  const authState = useSelector((state) => {state.auth});
+  const [isTokenThere, setIsTokenThere] = useState(false);
 
   useEffect(() => {
-    if(localStorage.getItem('token') === null){
+    if (localStorage.getItem("token") === null) {
       router.push("/login");
     }
-  })
+    setIsTokenThere(true);
+  }, []);
 
+  useEffect(() => {
+    if (isTokenThere) {
+      dispatch(getAllPosts());
+      dispatch(getAboutUser({ token: localStorage.getItem("token") }));
+    }
+  }, [isTokenThere]);
 
-
-  return (
-    <div>
-      Dashboard
-    </div>
-  );
+  return <div>
+    hey
+  </div>;
 }
