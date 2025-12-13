3682f25a7615a966e1ebbc71ea9363fa216ddf45
Dev aadi <adityasaini35947@gmail.com>
2025-12-13 17:30:37 +0530

    modified:   frontend/src/Components/Navbar/index.jsx

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles.module.css";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
 export default function NavBarComponent() {
+  const router = useRouter();
 
-    const router = useRouter();
+  const authState = useSelector((state) => state.auth);
 
   return (
     <div className={styles.container}>
       <nav className={styles.navBar}>
-
-        <h1 style={{cursor: "pointer"}} onClick={() => {
-          router.push("/");
-        }}>Pro connect</h1>
+        <h1
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          Pro connect
+        </h1>
 
         <div className={styles.navBarOptionContainer}>
-          <div onClick={() => {
-            router.push("/login")
-          }} className={styles.buttonJoin}>
-            <p>Be a part</p>
-          </div>
-        </div>
+          {authState.profileFetched && (
+            <div>
+              <div style={{display: "flex", gap: "1.2rem"}}>
+                <p>Hey, {authState.user.userId.name}</p>
+              <p style={{fontWeight: "bold", cursor: "pointer"}}>Profile</p>
+              </div>
+              
+            </div>
+          )}
 
+          {!authState.profileFetched && (
+            <div
+              onClick={() => {
+                router.push("/login");
+              }}
+              className={styles.buttonJoin}
+            >
+              <p>Be a part</p>
+            </div>
+          )}
+        </div>
       </nav>
     </div>
-
-    
-  )
+  );
 }
