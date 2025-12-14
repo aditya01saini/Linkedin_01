ac63145e45b231dec81510c44eae5bfc9758dbdf
Dev aadi <adityasaini35947@gmail.com>
2025-12-14 21:45:16 +0530

    add:   frontend/src/pages/discover/

---
+++ frontend/src/pages/discover/index.jsx
@@ -0,0 +1,20 @@
+import DashboardLayout from '@/layout/DashboardLayout';
+import UserLayout from '@/layout/UserLayout';
+import React from 'react';
+
+export default function DiscoverPage() {
+  return (
+    <div>
+       <UserLayout>
+      
+          <DashboardLayout>
+            <div>
+              <h1>Discover</h1>
+            </div>
+          </DashboardLayout>
+      
+          </UserLayout>
+          
+    </div>
+  );
+}
