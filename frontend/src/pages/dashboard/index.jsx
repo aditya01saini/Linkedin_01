28ef0e948c483cdd96deb7c7ff439df6adf7f112
Dev aadi <adityasaini35947@gmail.com>
2025-12-14 21:44:57 +0530

    modified:   frontend/src/pages/dashboard/index.jsx

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -5,34 +5,31 @@ import UserLayout from "@/layout/UserLayout";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+// import { setTokenIsNotThere } from "@/config/redux/reducer/authReducer";
+// import { setTokenIsThere } from "@/config/redux/reducer/authReducer";
 
 export default function dashboard() {
   const router = useRouter();
   const dispatch = useDispatch();
 
   const authState = useSelector((state) => state.auth);
-  const [isTokenThere, setIsTokenThere] = useState(false);
+  // const [isTokenThere, setIsTokenThere] = useState(false);
+
+
 
   useEffect(() => {
-    if (localStorage.getItem("token") === null) {
-      router.push("/login");
-    }
-    setIsTokenThere(true);
-  })
-
-  useEffect(() => {
-    if (isTokenThere) {
+    if (authState.isTokenThere) {
       dispatch(getAllPosts());
       dispatch(getAboutUser({ token: localStorage.getItem("token") }));
     }
-  }, [isTokenThere]);
+  }, [authState.isTokenThere]);
 
   return (
     <UserLayout>
 
     <DashboardLayout>
       <div>
-        <h1>Dashboard</h1>
+        <h1>Dashbord</h1>
       </div>
     </DashboardLayout>
 
