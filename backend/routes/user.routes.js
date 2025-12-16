029a7a586fba97a2672e5a77c99358c51e75468b
Dev aadi <adityasaini35947@gmail.com>
2025-12-16 21:35:59 +0530

    modified:   backend/routes/user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -36,7 +36,7 @@ router
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/user_update").post(updateUserProfile); // update: username, email
-router.route("/get_user_and_Profile").get(getUserAndProfile);
+router.route("/get_user_and_profile").get(getUserAndProfile);
 router.route("/update_profile_data").post(updateProfileData);
 router.route("/user/get_all_users").get(getAllUserProfile);
 router.route("/user/download_resume").get(downloadProfile);
