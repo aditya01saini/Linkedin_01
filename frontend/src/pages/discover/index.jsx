e9ba313902ec4acf4eeed39da33b3657a4886993
Dev aadi <adityasaini35947@gmail.com>
2025-12-18 03:40:56 +0530

    modified:   frontend/src/pages/discover/index.jsx

--- frontend/src/pages/discover/index.jsx
+++ frontend/src/pages/discover/index.jsx
@@ -1,8 +1,22 @@
+import { getAllUsers } from '@/config/redux/action/authAction';
 import DashboardLayout from '@/layout/DashboardLayout';
 import UserLayout from '@/layout/UserLayout';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function DiscoverPage() {
+
+  const authState = useSelector((state) => state.auth)
+
+  const dispatch = useDispatch();
+useEffect(() => {
+
+  if(!authState.all_profiles_fetched) {
+    dispatch(getAllUsers());
+  }
+
+}, [])
+
   return (
     <div>
        <UserLayout>
