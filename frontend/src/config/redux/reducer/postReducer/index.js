1882d18bc3ecf7703f2bb60b7f21186e03bd2677
Dev aadi <adityasaini35947@gmail.com>
2026-01-16 09:37:49 +0530

    modified:   frontend/src/config/redux/reducer/postReducer/index.js

--- frontend/src/config/redux/reducer/postReducer/index.js
+++ frontend/src/config/redux/reducer/postReducer/index.js
@@ -41,6 +41,7 @@ const postSlice = createSlice({
       .addCase(getAllComments.fulfilled, (state, action) => {
         state.postId = action.payload.post_id,
         state.comments = action.payload.comment
+        state.comments = action.payload.comment
       })
   },
 });
