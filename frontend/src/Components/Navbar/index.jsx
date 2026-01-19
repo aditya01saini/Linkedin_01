5279e9925cb5d9cecb5b712bd66e23b69024625d
Dev aadi <adityasaini35947@gmail.com>
2026-01-19 18:31:21 +0530

    modified:   frontend/src/Components/Navbar/index.jsx

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -30,7 +30,9 @@ export default function NavBarComponent() {
               <div style={{display: "flex", gap: "1.2rem"}}>
                 <p>Hey, {authState.user.userId.name}</p>
                 
-              <p style={{fontWeight: "bold", cursor: "pointer"}}>Profile</p>
+              <p onClick={() => {
+                router.push("/profile")
+              }} style={{fontWeight: "bold", cursor: "pointer"}}>Profile</p>
               <p onClick={() => {
                 localStorage.removeItem("token");
                 router.push("/login");
