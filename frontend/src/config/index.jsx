8f4fea3663abf97e6ce153b59e16f33c5decf248
Dev aadi <adityasaini35947@gmail.com>
2026-01-21 15:55:49 +0530

    modified:   frontend/src/config/index.jsx

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default: axios} = require("axios");
 
-export const BASE_URL = "http://localhost:9090" 
+export const BASE_URL = "https://linkedin-01.onrender.com" 
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
